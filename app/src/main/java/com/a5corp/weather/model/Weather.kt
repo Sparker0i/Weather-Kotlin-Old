f0b8e8c99d447583a80c51907858fe00a694139b
Aaditya Menon <aaditya@sparker0i.me>
2019-11-19 21:15:29 +0530

    Finished with Data Parse (#3)
    * Added More Constants
    
    * Moved back to Stable Android Studio
    
    * Working Data Fetch
    
    * Working Data Parse

--- app/src/main/java/com/a5corp/weather/model/Weather.kt
+++ app/src/main/java/com/a5corp/weather/model/Weather.kt
@@ -4,67 +4,57 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
+data class Clouds(
+    val all: Int
+)
+
+@JsonClass(generateAdapter = true)
 data class CurrentWeather(
+    @Json(name = "clouds") val clouds: Clouds,
+    @Json(name = "cod") val status: Int,
     @Json(name = "coord") val coordinates: Coordinates,
+    @Json(name = "dt") val timestamp: Int,
+    @Json(name = "id") val id: Int,
+    @Json(name = "main") val weather: Weather,
+    @Json(name = "name") val name: String,
+    @Json(name = "sys") val sys: Sys,
+    @Json(name = "visibility") val visibility: Int,
     @Json(name = "weather") val condition: List<Condition>,
-    @Json(name = "base") val base: String,
-    @Json(name = "main") val weatherCondition: Weather,
-    @Json(name = "wind") val windCondition: Wind,
-    @Json(name = "clouds") val cloudCondition: Cloud,
-    @Json(name = "rain") val rainCondition: Rain,
-    @Json(name = "snow") val snowCondition: Snow,
-    @Json(name = "dt") val date: Double,
-    @Json(name = "sys") val sysCondition: Sys,
-    @Json(name = "id") val cityId: Long,
-    @Json(name = "name") val cityName: String,
-    @Json(name = "cod") val status: Int
+    @Json(name = "wind") val wind: Wind
 )
 
 @JsonClass(generateAdapter = true)
 data class Coordinates(
-    @Json(name = "lat") val latitude: String,
-    @Json(name = "lon") val longitude: String
-)
-@JsonClass(generateAdapter = true)
-data class Condition(
-    @Json(name = "id") val id: Int,
-    @Json(name = "description") val description: String
+    @Json(name = "lat") val lat: Double,
+    @Json(name = "lon") val lon: Double
 )
 
 @JsonClass(generateAdapter = true)
 data class Weather(
+    @Json(name = "humidity") val humidity: Int,
+    @Json(name = "pressure") val pressure: Int,
     @Json(name = "temp") val temperature: Double,
-    @Json(name = "pressure") val pressure: Double,
-    @Json(name = "humidity") val humidity: Double,
-    @Json(name = "temp_min") val minimumTemperature: Double,
-    @Json(name = "temp_max") val maximumTemperature: Double
-)
-
-@JsonClass(generateAdapter = true)
-data class Wind(
-    @Json(name = "speed") val speed: Double,
-    @Json(name = "deg") val degree: Double
-)
-
-@JsonClass(generateAdapter = true)
-data class Cloud(@Json(name = "all") val cloudiness: Double)
-
-@JsonClass(generateAdapter = true)
-data class Rain(
-    @Json(name = "1h") val oneHour: Double,
-    @Json(name = "3h") val threeHour: Double
-)
-
-@JsonClass(generateAdapter = true)
-data class Snow(
-    @Json(name = "1h") val oneHour: Double,
-    @Json(name = "3h") val threeHour: Double
+    @Json(name = "temp_max") val maxTemperature: Double,
+    @Json(name = "temp_min") val minTemperature: Int
 )
 
 @JsonClass(generateAdapter = true)
 data class Sys(
-    @Json(name = "message") val message: Double,
     @Json(name = "country") val country: String,
+    @Json(name = "id") val id: Int,
     @Json(name = "sunrise") val sunrise: Long,
-    @Json(name = "sunset") val sunset: Long
+    @Json(name = "sunset") val sunset: Int,
+    @Json(name = "type") val type: Int
+)
+
+@JsonClass(generateAdapter = true)
+data class Condition(
+    @Json(name = "description") val description: String,
+    @Json(name = "id") val id: Int
+)
+
+@JsonClass(generateAdapter = true)
+data class Wind(
+    @Json(name = "deg") val degree: Int,
+    @Json(name = "speed") val speed: Double
 )
